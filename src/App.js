11c73688994bbbffec89a0a3dd6ef73e5016abd4
MohammadZeeshanQ <53648401+MohammadZeeshanQ@users.noreply.github.com>
2022-04-04 02:19:55 +0500

    Git Init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+	// state value
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
